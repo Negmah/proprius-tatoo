d7eb0ddbe970b4425b98040aaef6d02e38db8098
Monica Moura <monica.a.moura@hotmail.com>
2018-09-17 00:48:51 +0100

    Add meta tags for bootstrap and fontawesome, created Navbar component

--- proprius-tattoo/src/App.js
+++ proprius-tattoo/src/App.js
@@ -1,17 +1,13 @@
 import React, { Component } from 'react';
 import './App.css';
-import './Homepage.js';
 import Homepage from './Homepage.js';
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-            <Homepage />
-</div>
+      <div className="App" style={{width: "100vw", height: "100vh"}}>
+      <Homepage />
+      </div>
     );
   }
 }
--- proprius-tattoo/src/Homepage.js
+++ proprius-tattoo/src/Homepage.js
@@ -3,8 +3,8 @@ import React, { Component } from 'react';
 class Homepage extends Component {
     render() {
         return (
-            <div>
-                Hello world
+            <div id="homepage" style={{backgroundColor: "black", width: "100vw", height: "100vh"}}>
+                <img className="img-responsive col-xs-12" src="https://res.cloudinary.com/negmah/image/upload/v1536972745/skull_king.jpg" alt="vintage play card with skull king in black background"></img>
             </div>
         )
     }
